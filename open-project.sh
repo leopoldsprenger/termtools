dfb9f9bb570da63a87babe8db5728ccaa8766c71
Leopold Sprenger <186564656+leopoldsprenger@users.noreply.github.com>
2026-02-21 18:38:35 +0100

    feat: add open-project.sh script to fuzzy find and open a project in neovim

---
+++ open-project.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+PROJECTS_DIR="$HOME/Documents/projects"
+
+SELECTION=$(find "$PROJECTS_DIR" -mindepth 2 -maxdepth 2 -type d \
+    | grep -v '/\.' \
+    | sed "s|^$PROJECTS_DIR/||" \
+    | fzf --prompt="Select project: ")
+
+if [ -n "$SELECTION" ]; then
+    nvim -c "cd $PROJECTS_DIR/$SELECTION"
+fi
